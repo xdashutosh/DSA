e007db874c9ac1fed98c4d267b0682353434fda8
ashutosh chaudhary <ashuchaudhary6969@gmail.com>
2023-04-09 15:09:27 +0530

    switch case

---
+++ SwitchCase.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ int value;
+cin>>value;
+    switch (value)
+    {
+    case 1:
+    cout<<"you entered 1";
+        break;
+     case 2: 
+     cout<<"you entered 2";
+     break;  
+    
+    default:
+    cout<<"you entered other than 1 and 2";
+        break;
+    }
+return 0;
+}
