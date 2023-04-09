80db5904de69cab5c96302255e745659858d1100
ashutosh chaudhary <ashuchaudhary6969@gmail.com>
2023-04-09 19:54:57 +0530

    array basics completed

---
+++ array/array_basics.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[3]={1,2,3};
+    for (int i = 0; i < 3; i++)
+    {
+     cout<<a[i]<<" ";
+    }
+
+    cout<<endl<<"array length "<<sizeof(a)/sizeof(int)<<endl;
+    
+    char characters[4]={'a','s','h','u'};
+    for (int i = 0; i <4; i++)
+    {
+     cout<<characters[i]<<" ";
+        
+    }
+    
+return 0;
+}
